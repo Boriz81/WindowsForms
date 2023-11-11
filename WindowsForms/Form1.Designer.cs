2802c7c6ebd5948401a19a601a91fab6374d72ad
boriz81 <blitzboriz@gmail.com>
2023-11-11 23:52:32 +0900

    DONE 11/11/23

--- WindowsForms/Form1.Designer.cs
+++ WindowsForms/Form1.Designer.cs
@@ -33,13 +33,16 @@
 			this.label1 = new System.Windows.Forms.Label();
 			this.cbShowDate = new System.Windows.Forms.CheckBox();
 			this.timer1 = new System.Windows.Forms.Timer(this.components);
-			this.hd_button = new System.Windows.Forms.Button();
-			this.cl_button = new System.Windows.Forms.Button();
+			this.btnHideControls = new System.Windows.Forms.Button();
+			this.btnClose = new System.Windows.Forms.Button();
 			this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
 			this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
 			this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+			this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
 			this.showDateToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
 			this.hideContrloToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.toolStripSeparator3 = new System.Windows.Forms.ToolStripSeparator();
 			this.closeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
 			this.bt_mini = new System.Windows.Forms.Button();
 			this.contextMenuStrip1.SuspendLayout();
@@ -49,8 +52,9 @@
 			// 
 			this.label1.AutoSize = true;
 			this.label1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+			this.label1.ContextMenuStrip = this.contextMenuStrip1;
 			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-			this.label1.Location = new System.Drawing.Point(518, 9);
+			this.label1.Location = new System.Drawing.Point(48, 9);
 			this.label1.Name = "label1";
 			this.label1.Size = new System.Drawing.Size(192, 73);
 			this.label1.TabIndex = 0;
@@ -62,7 +66,7 @@
 			// 
 			this.cbShowDate.AutoSize = true;
 			this.cbShowDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-			this.cbShowDate.Location = new System.Drawing.Point(61, 319);
+			this.cbShowDate.Location = new System.Drawing.Point(61, 172);
 			this.cbShowDate.Name = "cbShowDate";
 			this.cbShowDate.Size = new System.Drawing.Size(192, 41);
 			this.cbShowDate.TabIndex = 1;
@@ -77,33 +81,32 @@
 			this.timer1.Interval = 1000;
 			this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
 			// 
-			// hd_button
+			// btnHideControls
 			// 
-			this.hd_button.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-			this.hd_button.Location = new System.Drawing.Point(61, 382);
-			this.hd_button.Name = "hd_button";
-			this.hd_button.Size = new System.Drawing.Size(205, 43);
-			this.hd_button.TabIndex = 2;
-			this.hd_button.Text = "Hide Control";
-			this.hd_button.UseVisualStyleBackColor = true;
-			this.hd_button.Click += new System.EventHandler(this.hd_button_Click);
+			this.btnHideControls.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.btnHideControls.Location = new System.Drawing.Point(61, 219);
+			this.btnHideControls.Name = "btnHideControls";
+			this.btnHideControls.Size = new System.Drawing.Size(278, 52);
+			this.btnHideControls.TabIndex = 2;
+			this.btnHideControls.Text = "Hide Control";
+			this.btnHideControls.UseVisualStyleBackColor = true;
+			this.btnHideControls.Click += new System.EventHandler(this.hd_button_Click);
 			// 
-			// cl_button
+			// btnClose
 			// 
-			this.cl_button.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-			this.cl_button.Location = new System.Drawing.Point(587, 382);
-			this.cl_button.Name = "cl_button";
-			this.cl_button.Size = new System.Drawing.Size(122, 43);
-			this.cl_button.TabIndex = 3;
-			this.cl_button.Text = "Close";
-			this.cl_button.UseVisualStyleBackColor = true;
-			this.cl_button.Click += new System.EventHandler(this.cl_button_Click);
+			this.btnClose.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.btnClose.Location = new System.Drawing.Point(61, 277);
+			this.btnClose.Name = "btnClose";
+			this.btnClose.Size = new System.Drawing.Size(278, 52);
+			this.btnClose.TabIndex = 3;
+			this.btnClose.Text = "Close";
+			this.btnClose.UseVisualStyleBackColor = true;
+			this.btnClose.Click += new System.EventHandler(this.cl_button_Click);
 			// 
 			// notifyIcon1
 			// 
 			this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
 			this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
-			this.notifyIcon1.Text = "notifyIcon1";
 			this.notifyIcon1.Visible = true;
 			this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
 			// 
@@ -111,32 +114,52 @@
 			// 
 			this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.toolStripMenuItem1,
+            this.toolStripSeparator1,
             this.showDateToolStripMenuItem,
+            this.toolStripSeparator2,
             this.hideContrloToolStripMenuItem,
+            this.toolStripSeparator3,
             this.closeToolStripMenuItem});
 			this.contextMenuStrip1.Name = "contextMenuStrip1";
-			this.contextMenuStrip1.Size = new System.Drawing.Size(181, 114);
+			this.contextMenuStrip1.Size = new System.Drawing.Size(161, 110);
 			this.contextMenuStrip1.Opening += new System.ComponentModel.CancelEventHandler(this.contextMenuStrip1_Opening);
 			// 
 			// toolStripMenuItem1
 			// 
+			this.toolStripMenuItem1.CheckOnClick = true;
 			this.toolStripMenuItem1.Name = "toolStripMenuItem1";
 			this.toolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
 			this.toolStripMenuItem1.Text = "Show Controls";
 			this.toolStripMenuItem1.Click += new System.EventHandler(this.toolStripMenuItem1_Click);
 			// 
+			// toolStripSeparator1
+			// 
+			this.toolStripSeparator1.Name = "toolStripSeparator1";
+			this.toolStripSeparator1.Size = new System.Drawing.Size(177, 6);
+			// 
 			// showDateToolStripMenuItem
 			// 
+			this.showDateToolStripMenuItem.CheckOnClick = true;
 			this.showDateToolStripMenuItem.Name = "showDateToolStripMenuItem";
 			this.showDateToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
 			this.showDateToolStripMenuItem.Text = "Show date";
 			this.showDateToolStripMenuItem.Click += new System.EventHandler(this.showDateToolStripMenuItem_Click);
+			// 
+			// toolStripSeparator2
+			// 
+			this.toolStripSeparator2.Name = "toolStripSeparator2";
+			this.toolStripSeparator2.Size = new System.Drawing.Size(177, 6);
 			// 
 			// hideContrloToolStripMenuItem
 			// 
 			this.hideContrloToolStripMenuItem.Name = "hideContrloToolStripMenuItem";
 			this.hideContrloToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
 			this.hideContrloToolStripMenuItem.Text = "Hide Control";
+			// 
+			// toolStripSeparator3
+			// 
+			this.toolStripSeparator3.Name = "toolStripSeparator3";
+			this.toolStripSeparator3.Size = new System.Drawing.Size(177, 6);
 			// 
 			// closeToolStripMenuItem
 			// 
@@ -148,9 +171,9 @@
 			// bt_mini
 			// 
 			this.bt_mini.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-			this.bt_mini.Location = new System.Drawing.Point(587, 319);
+			this.bt_mini.Location = new System.Drawing.Point(61, 335);
 			this.bt_mini.Name = "bt_mini";
-			this.bt_mini.Size = new System.Drawing.Size(123, 32);
+			this.bt_mini.Size = new System.Drawing.Size(278, 52);
 			this.bt_mini.TabIndex = 4;
 			this.bt_mini.Text = "Minimized\r\n\r\n";
 			this.bt_mini.UseVisualStyleBackColor = true;
@@ -161,18 +184,18 @@
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.BackColor = System.Drawing.SystemColors.Control;
-			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.ClientSize = new System.Drawing.Size(465, 450);
 			this.Controls.Add(this.bt_mini);
-			this.Controls.Add(this.cl_button);
-			this.Controls.Add(this.hd_button);
+			this.Controls.Add(this.btnClose);
+			this.Controls.Add(this.btnHideControls);
 			this.Controls.Add(this.cbShowDate);
 			this.Controls.Add(this.label1);
 			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
 			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-			this.Location = new System.Drawing.Point(1100, 0);
 			this.Name = "Form1";
 			this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
 			this.Text = "Form1";
+			this.TopMost = true;
 			this.TransparencyKey = System.Drawing.SystemColors.Control;
 			this.Load += new System.EventHandler(this.Form1_Load);
 			this.contextMenuStrip1.ResumeLayout(false);
@@ -186,8 +209,8 @@
 		private System.Windows.Forms.Label label1;
 		private System.Windows.Forms.CheckBox cbShowDate;
 		private System.Windows.Forms.Timer timer1;
-		private System.Windows.Forms.Button hd_button;
-		private System.Windows.Forms.Button cl_button;
+		private System.Windows.Forms.Button btnHideControls;
+		private System.Windows.Forms.Button btnClose;
 		private System.Windows.Forms.NotifyIcon notifyIcon1;
 		private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
 		private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
@@ -195,6 +218,9 @@
 		private System.Windows.Forms.ToolStripMenuItem hideContrloToolStripMenuItem;
 		private System.Windows.Forms.ToolStripMenuItem closeToolStripMenuItem;
 		private System.Windows.Forms.Button bt_mini;
+		private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+		private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+		private System.Windows.Forms.ToolStripSeparator toolStripSeparator3;
 	}
 }
 
--- WindowsForms/Form1.cs
+++ WindowsForms/Form1.cs
@@ -13,17 +13,47 @@ namespace WindowsForms
 {
 	public partial class Form1 : Form
 	{
+		bool visible_controls;
+		bool show_date;
 		
 		public Form1()
 		{
 			
 			InitializeComponent();
-			this.hd_button.Visible = false;
-			this.cl_button.Visible = false;
+			this.Location = new System.Drawing.Point
+				(
+				System.Windows.Forms.Screen.PrimaryScreen.Bounds.Right - this.Width - 50,
+				System.Windows.Forms.Screen.PrimaryScreen.Bounds.Top + 100
+				);
+			show_date = false;
+			visible_controls = false;
+			btnHideControls.Visible = false;
+			btnClose.Visible = false;
+			this.btnHideControls.Visible = false;
+			this.btnClose.Visible = false;
 			this.bt_mini.Visible = false;
 			this.SizeChanged += Form1_Resize;
 		}
+		private void SetShowDate(bool show_date)
+		{
+			this.show_date = show_date;
+			cbShowDate.Checked = show_date;
+			showDateToolStripMenuItem.Checked = show_date;
+		}
+		private void SetControlsVisibility(bool visible_controls)
+		{
+			this.visible_controls = visible_controls;
+			this.FormBorderStyle = visible_controls ? FormBorderStyle.Sizable : FormBorderStyle.None;
+			this.TransparencyKey = visible_controls ? Color.AliceBlue : SystemColors.Control;
 
+			this.ShowInTaskbar = visible_controls;
+			this.cbShowDate.Visible = visible_controls;
+
+			this.btnHideControls.Visible = visible_controls;
+			this.btnClose.Visible = visible_controls;
+			//this.notifyIcon1.Visible = !visible_controls;
+			this.toolStripMenuItem1.Checked = visible_controls;
+		}
 		private void Form1_Resize(object sender, EventArgs e)
 		{
 			if (this.WindowState == FormWindowState.Minimized)
@@ -34,7 +64,7 @@ namespace WindowsForms
 			}
 			else if (FormWindowState.Normal == WindowState)
 			{
-				notifyIcon1.Visible= false;
+				notifyIcon1.Visible= true;
 			}
 		}
 		private void label1_Click(object sender, EventArgs e)
@@ -46,41 +76,48 @@ namespace WindowsForms
 		{
 			label1.Text = DateTime.Now.ToString("hh.mm.ss tt");
 			if(cbShowDate.Checked )label1.Text+=$"\n{DateTime.Now.ToString("dd.MM.yyyy")}";
+			notifyIcon1.Text = DateTime.Now.ToString("hh.mm.ss");
 		}
 
 		private void label1_DoubleClick(object sender, EventArgs e)
 		{
-			this.FormBorderStyle = FormBorderStyle.Sizable;
+			/*this.FormBorderStyle = FormBorderStyle.Sizable;
 			this.TransparencyKey = Color.AliceBlue;
 			this.ShowInTaskbar = true;
 			this.cbShowDate.Visible = true;
-			this.hd_button.Visible = true;
+			this.btnHideControls.Visible = true;
 			this.BackColor = Color.White;
-			this.cl_button.Visible = true;
+			this.btnClose.Visible = true;
 			this.bt_mini.Visible = true;
+			this.notifyIcon1.Visible = false;*/
+			SetControlsVisibility(true);
 		}
 
 		private void cbShowDate_CheckedChanged(object sender, EventArgs e)
 		{
-
+			SetShowDate(cbShowDate.Checked);
+			
 		}
 
 		private void hd_button_Click(object sender, EventArgs e) 
 		{
-			
-			this.FormBorderStyle = FormBorderStyle.None;
-			this.ShowInTaskbar = false;
+
+			/*this.FormBorderStyle = FormBorderStyle.None;*/
+			//this.TransparencyKey = SystemColors.Control;
+			/*this.ShowInTaskbar = false;
 			this.cbShowDate.Visible = false;
-			this.hd_button.Visible = false;
+			this.btnHideControls.Visible = false;
 			this.BackColor = TransparencyKey;
-			this.cl_button.Visible = false;
+			this.btnClose.Visible = false;
 			this.bt_mini.Visible = false;
-			
+			this.notifyIcon1.Visible = true;*/
+			SetControlsVisibility(false);
+
 		}
 
 		private void cl_button_Click(object sender, EventArgs e)
 		{
-			Close();
+			this.Close();
 		}
 
 		private void Form1_Load(object sender, EventArgs e)
@@ -88,23 +125,27 @@ namespace WindowsForms
 			notifyIcon1.Visible = false;
 			notifyIcon1.BalloonTipTitle = "Clock";
 			notifyIcon1.BalloonTipText = "SystemTray";
-			notifyIcon1.Text = "Clock";
+			
 		}
 
 		private void showDateToolStripMenuItem_Click(object sender, EventArgs e)
 		{
-			this.Show();
+			
+			this.show_date = showDateToolStripMenuItem.Checked;
+			SetShowDate(show_date);
+
+
+			/*this.Show();
 			this.WindowState = FormWindowState.Normal;
 			notifyIcon1.Visible = false;
 			this.FormBorderStyle = FormBorderStyle.None;
 			this.ShowInTaskbar = false;
-			this.cbShowDate.Visible = false;
-			this.hd_button.Visible = false;
+			
+			this.btnHideControls.Visible = false;
 			this.BackColor = TransparencyKey;
-			this.cl_button.Visible = false;
-			this.bt_mini.Visible = false;
-			label1.Text = DateTime.Now.ToString("hh.mm.ss tt");
-			if (cbShowDate.Checked) label1.Text += $"\n{DateTime.Now.ToString("dd.MM.yyyy")}";
+			this.btnClose.Visible = false;
+			this.bt_mini.Visible = false;*/
+
 		}
 
 		private void contextMenuStrip1_Opening(object sender, CancelEventArgs e)
@@ -114,37 +155,37 @@ namespace WindowsForms
 
 		private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
 		{
-			this.Show();
-			notifyIcon1.Visible=false;
-			this.WindowState = FormWindowState.Normal;
+			label1_DoubleClick(sender, e);
 		}
 
 		private void closeToolStripMenuItem_Click(object sender, EventArgs e)
 		{
-			notifyIcon1.Visible=false;
-			notifyIcon1.ContextMenuStrip.Visible=false;
-			notifyIcon1.ContextMenuStrip.Close();	
+			
 			this.Close();
 		}
 
 		private void toolStripMenuItem1_Click(object sender, EventArgs e)
 		{
-			this.Show();
-			notifyIcon1.Visible = false;
-			this.FormBorderStyle = FormBorderStyle.Sizable;
-			this.TransparencyKey = Color.AliceBlue;
+			if(toolStripMenuItem1.Checked) label1_DoubleClick(sender, e);
+			else hd_button_Click(sender, e);
+
+			/*this.Show();
+			notifyIcon1.Visible = true;
+			this.FormBorderStyle = FormBorderStyle.None;
+			this.BackColor = TransparencyKey;
 			this.ShowInTaskbar = true;
 			this.cbShowDate.Visible = true;
-			this.hd_button.Visible = true;
-			this.BackColor = Color.White;
-			this.cl_button.Visible = true;
+			this.btnHideControls.Visible = true;
+			this.btnClose.Visible = true;
 			this.bt_mini.Visible = true;
-			this.WindowState = FormWindowState.Normal;
+			this.WindowState = FormWindowState.Normal;*/
 		}
 
 		private void bt_mini_Click(object sender, EventArgs e)
 		{
 			this.WindowState = FormWindowState.Minimized;
 		}
+
+		
 	}
 }
